aee6e2e6d7e74ee2caa5d4e8271122827af440b2
Hayk <haykvadan@gmail.com>
2024-12-31 17:52:31 -0800

    Reworked script to make it more readable and fixed some bugs

--- src/AutoCraft/AutoSturgeon.java
+++ src/AutoCraft/AutoSturgeon.java
@@ -12,6 +12,9 @@ import org.dreambot.api.methods.grandexchange.GrandExchange;
 import org.dreambot.api.methods.grandexchange.GrandExchangeItem;
 import org.dreambot.api.methods.grandexchange.LivePrices;
 
+import core.BankHandler;
+
+
 
 
 
@@ -19,6 +22,7 @@ import org.dreambot.api.methods.grandexchange.LivePrices;
         version = 1.0, category = Category.WOODCUTTING, image = "")
 public class AutoSturgeon extends AbstractScript {
 
+    BankHandler bankHandler = new BankHandler();
     @Override
     public int onLoop() {
 
@@ -26,7 +30,7 @@ public class AutoSturgeon extends AbstractScript {
         String itemToSell = "Caviar";
 
         // This block checks if we are out of stock, and if so sells what we have and buys more stock.
-        if (!Inventory.isFull() && bankIsOutOfStock(itemToWithdraw)) {
+        if (!Inventory.isFull() && bankHandler.isOutOfStock(itemToWithdraw)) {
             sleep(250);
             stop(); // Temporarily
             // startGE(itemToWithdraw, itemToSell);
@@ -35,7 +39,7 @@ public class AutoSturgeon extends AbstractScript {
         // This block handles the case where we need to restock our inventory.
         else if (!Inventory.isFull()) {
             Logger.log("Inventory is not full");
-            bankHandlerEmptyInventory(itemToWithdraw);
+            bankHandler.restock(itemToWithdraw);
         }
 
         Item knife = Inventory.get("Knife");
@@ -52,87 +56,23 @@ public class AutoSturgeon extends AbstractScript {
                 // This if block prevents a duplicate cut of slot 26 sometimes it executes to fast.
                 if(fish != null) {
                     fish.interact("Use");
+
+                }
+                else {
+                    break;
                 }
 
-            } while (Inventory.contains("Leaping sturgeon"));
+            } while (fish.getName().equals(itemToWithdraw));
 
             Logger.log("Finished Crafting...");
             sleep(500);
 
             // Crafting is done... So now it will deposit our depositItem into the bank
             Item depositItem = Inventory.get("Caviar");
-            if (!Inventory.contains("Leaping sturgeon")) {
-                bankHandler(depositItem, itemToWithdraw);
-            }
+            bankHandler.depositAndWithdraw(depositItem, itemToWithdraw);
         }
-
 
         return 1000; // Pause for 1 second
-    }
-
-
-    public Boolean bankIsOutOfStock(String checkFor) {
-        if (!Bank.isOpen()) {
-            Bank.open();
-        }
-
-        boolean res = !Bank.contains(checkFor);
-        Logger.log("Item you are looking for: " + checkFor + " is out of stock = " + res);
-        sleep(2000);
-        return res;
-    }
-    public void bankHandlerEmptyInventory(String itemToWithdraw) {
-        if (!Bank.open()) {
-            Bank.open();
-        }
-
-        Item withdraw = null;
-
-        if (Bank.contains(itemToWithdraw)) {
-            withdraw = Bank.get(itemToWithdraw);
-        }
-
-
-        if (withdraw != null) {
-            Bank.withdrawAll(withdraw.getName());
-        }
-
-        Bank.close();
-    }
-    public void bankHandler(Item deposit, String itemToWithdraw) {
-        // Ensure the bank is open
-        Item withdraw = null;
-
-        if (!Bank.isOpen()) {
-            Bank.open();
-
-            if (Bank.contains(itemToWithdraw)) {
-                withdraw = Bank.get(itemToWithdraw);
-            }
-        }
-
-        if (deposit != null) {
-
-            Logger.log("Item found depositing all in bank");
-            Bank.depositAll(deposit);
-        }
-
-        Logger.log("Checking bank to see if withdrawable item is available... ");
-        // sleep(1000);
-        if (withdraw != null && Bank.contains(withdraw.getName())) {
-
-            Logger.log("Item to withdraw found... withdrawing all");
-            Bank.withdrawAll(withdraw.getName());
-            // sleep(500);
-        } else if (withdraw == null) {
-            Logger.log("Cannot withdraw: Item is null.");
-            // sleep(5000);
-        } else if (!Bank.contains(withdraw.getName())) {
-            Logger.log("The item to withdraw is not available in the bank.");
-            // sleep(5000);
-        }
-
-        Bank.close();
     }
 
     // This method handles resupplying gold, and restocking item to process.
