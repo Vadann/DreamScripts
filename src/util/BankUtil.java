395b915d6ea467f46cfad2ac7fc3633fe15d4d81
Hayk <haykvadan@gmail.com>
2025-01-20 15:19:18 -0800

    Added more utility functions

--- src/util/BankUtil.java
+++ src/util/BankUtil.java
@@ -8,6 +8,7 @@ import org.dreambot.api.utilities.Logger;
 import org.dreambot.api.methods.walking.impl.Walking;
 import org.dreambot.api.utilities.Sleep;
 import org.dreambot.api.methods.map.Area;
+import org.dreambot.api.wrappers.items.Item;
 
 public class BankUtil {
 
@@ -71,5 +72,97 @@ public class BankUtil {
 
     public static boolean hasDepositedEverything() {
         return !Inventory.contains(item -> true) && Equipment.isEmpty();
+    }
+
+    /**
+     * Checks if an item is out of stock in the bank
+     * @param itemName The name of the item to check
+     * @return true if the item count in bank is 0
+     */
+    public static boolean isOutOfStock(String itemName) {
+        if (!Bank.isOpen() && !Bank.open()) {
+            return true; // Consider out of stock if we can't check bank
+        }
+        return Bank.count(itemName) == 0;
+    }
+
+    /**
+     * Checks if player has enough coins in bank or inventory
+     * @param amount The amount of coins needed
+     * @return true if player has enough coins
+     */
+    public static boolean hasEnoughCoins(int amount) {
+        int inventoryCoins = Inventory.count("Coins");
+        if (inventoryCoins >= amount) {
+            return true;
+        }
+
+        if (!Bank.isOpen() && !Bank.open()) {
+            return false;
+        }
+
+        int bankCoins = Bank.count("Coins");
+        return (inventoryCoins + bankCoins) >= amount;
+    }
+
+    /**
+     * Gets specified amount of an item from bank
+     * @param itemName The name of the item to get
+     * @param amount The amount to withdraw
+     * @return true if successfully retrieved the item
+     */
+    public static boolean getItemFromBank(String itemName, int amount) {
+        if (!Bank.isOpen() && !Bank.open()) {
+            Logger.log("Failed to open bank");
+            return false;
+        }
+
+        // Check if we have enough of the item
+        if (Bank.count(itemName) < amount) {
+            Logger.log("Not enough " + itemName + " in bank");
+            return false;
+        }
+
+        // If inventory is full, deposit everything first
+        if (Inventory.isFull() && !itemName.equals("Coins")) {
+            Bank.depositAllItems();
+            Sleep.sleepUntil(() -> !Inventory.isFull(), 3000);
+        }
+
+        // Withdraw the item
+        boolean success = Bank.withdraw(itemName, amount);
+        Sleep.sleepUntil(() -> Inventory.contains(itemName), 3000);
+
+        return success && Inventory.contains(itemName);
+    }
+
+    /**
+     * Gets as many of the specified item from bank as possible
+     * @param itemName The name of the item to get
+     * @return true if successfully retrieved any amount of the item
+     */
+    public static boolean getItemFromBank(String itemName) {
+        if (!Bank.isOpen() && !Bank.open()) {
+            Logger.log("Failed to open bank");
+            return false;
+        }
+
+        // Check if the item exists in bank
+        if (Bank.count(itemName) == 0) {
+            Logger.log("No " + itemName + " found in bank");
+            return false;
+        }
+
+        // If inventory is full, deposit everything first (except coins for convenience)
+        if (Inventory.isFull() && !itemName.equals("Coins")) {
+            Bank.depositAllItems();
+            Sleep.sleepUntil(() -> !Inventory.isFull(), 3000);
+        }
+
+        // Withdraw as many as possible
+        boolean success = Bank.withdrawAll(itemName);
+        Sleep.sleepUntil(() -> Inventory.contains(itemName), 3000);
+
+        return success && Inventory.contains(itemName);
     }
 } 
